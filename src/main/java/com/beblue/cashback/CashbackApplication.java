9a5f373dca670cd6c8aa3b43b2a4a3fde7d0dc6e
marciosmelo <marciomelo@gmail.com>
2019-07-19 11:49:00 -0300

    Initial project

---
+++ src/main/java/com/beblue/cashback/CashbackApplication.java
@@ -0,0 +1,13 @@
+package com.beblue.cashback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashbackApplication.class, args);
+	}
+
+}
